44caff1b428c287ef1274ef2188423eb4bbc831c
archiltavdgiridze <achitavdgiridze@gmail.com>
2022-12-05 23:21:04 +0400

    minor fixes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const SEARCH_API =
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState([""]);
+  const [load, setLoad] = useState(false);
 
   const changeTheSearch = (event) => {
     setSearch(event.target.value);
@@ -112,4 +113,3 @@ const App = () => {
 };
 
 export default App;
-
